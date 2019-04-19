e574865d7ec4881e7f0c4375342f5635c87e50e1
Ivan <rekiu@yandex.ru>
2019-04-19 21:07:35 +0800

    Правка к 1 заданию

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -1,14 +1,15 @@
+function main() {
 'use strict';
 
-
 let Money = prompt('Ваш бюджет на месяц?', '1000'),
-    Time = prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD'),
-    Costs = prompt('Введите обязательную статью расходов в этом месяце', '500'),
-    Price = prompt('Во сколько обойдется?', '250'),
-    SubCosts = prompt('Введите обязательную статью расходов в этом месяце', '500'),
-    SubPrice = prompt('Во сколько обойдется?', '250');
+  MoneyTwo = Money/30,
+  Time = prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD'),
+  Costs = prompt('Введите обязательную статью расходов в этом месяце', '500'),
+  Price = prompt('Во сколько обойдется?', '250'),
+  SubCosts = prompt('Введите обязательную статью расходов в этом месяце', '500'),
+  SubPrice = prompt('Во сколько обойдется?', '250');
 let appData = {
-  budget: Money,
+  budget: MoneyTwo,
   timeData: Time,
   expenses: {},
   optionalExpenses: {},
@@ -21,8 +22,8 @@ appData.expenses[SubCosts] = SubPrice;
 
 console.log(appData);
 
+alert('Бюджет на 1 день ' + MoneyTwo + ' условных единиц');
 
+}
 
-alert('Бюджет на 1 день ' + Money/30 + ' условных единиц');
-
-
+main();
