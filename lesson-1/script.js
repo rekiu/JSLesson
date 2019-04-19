f8c36b696c16aa9b10c00dc44cf329c99713be8e
Ivan <rekiu@yandex.ru>
2019-04-20 00:12:17 +0800

    Первое задание

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -2,14 +2,13 @@ function main() {
 'use strict';
 
 let Money = prompt('Ваш бюджет на месяц?', '1000'),
-  MoneyTwo = Money/30,
   Time = prompt('Введите дату в формате YYYY-MM-DD', 'YYYY-MM-DD'),
   Costs = prompt('Введите обязательную статью расходов в этом месяце', '500'),
   Price = prompt('Во сколько обойдется?', '250'),
   SubCosts = prompt('Введите обязательную статью расходов в этом месяце', '500'),
   SubPrice = prompt('Во сколько обойдется?', '250');
 let appData = {
-  budget: MoneyTwo,
+  budget: Money,
   timeData: Time,
   expenses: {},
   optionalExpenses: {},
@@ -22,7 +21,7 @@ appData.expenses[SubCosts] = SubPrice;
 
 console.log(appData);
 
-alert('Бюджет на 1 день ' + MoneyTwo + ' условных единиц');
+alert('Бюджет на 1 день ' + appData.budget/30 + ' условных единиц');
 
 }
 
