16f74a5718ea95a588f6665d641a390994705b2e
Ivan <rekiu@yandex.ru>
2019-04-21 03:03:21 +0800

    первое задание

--- lesson-1/Hard/script.js
+++ lesson-1/Hard/script.js
@@ -1,10 +1,14 @@
 function main() {
 'use strict';
 
-let num = 33721;
-    sum = 3*3*7*2*1;
+let num = 33721,
+    sum = 3*3*7*2*1,
+    extent = 3,
+    subSum = sum ** extent;
     console.log(sum);
-    
+    console.log(subSum);
+
+   alert(20);
 }
 
 main();
