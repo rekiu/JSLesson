7fa1fb53b4c3106a0db1e341c76c0c189eeadeec
Ivan <rekiu@yandex.ru>
2019-04-22 04:00:28 +0800

    Сложное задание

--- lesson-1/Hard/script.js
+++ lesson-1/Hard/script.js
@@ -1,14 +1,28 @@
+/*jshint esversion: 7 */
 function main() {
 'use strict';
 
-let num = 33721,
-    sum = 3*3*7*2*1,
-    extent = 3,
-    subSum = sum ** extent;
-    console.log(sum);
-    console.log(subSum);
+let num = (33721);
+let arr = [3, 3, 7, 2, 1],
+result = arr.reduce(function(sum, current) {
+  return sum * current
+});
 
-   alert(20);
+console.log(result);
+
+let subResult = [ result**3];
+
+console.log(subResult);
+
+/*subResult не определяется - undefined, в чём причина пока не ясно. 
+Предупреждение 'Exponentiation operator' is only available in ES7 
+(use 'esversion: 7'). (W119) устранил, но не помогло.*/
+
+//console.log(subResult.join("") );
+
+alert(subResult[3]); 
+
+
+
 }
-
 main();
