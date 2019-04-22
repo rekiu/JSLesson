cf392a5358a6a57074d0765589a4c1fa3ba0afac
Ivan <rekiu@yandex.ru>
2019-04-22 21:13:59 +0800

    переделал через строку

--- lesson-1/Hard/script.js
+++ lesson-1/Hard/script.js
@@ -2,27 +2,21 @@
 function main() {
 'use strict';
 
-let num = (33721);
-let arr = [3, 3, 7, 2, 1],
-result = arr.reduce(function(sum, current) {
-  return sum * current
-});
+let num = 33721;
+    num = '' + num;
 
+let result = 1;
+
+for (let i = 0; i < num.length; i++) {
+  result = result * num[i];
+}
 console.log(result);
 
-let subResult = [ result**3];
+let subResult = result ** 3 +'';
 
-console.log(subResult);
+//console.log(subResult);
 
-/*subResult не определяется - undefined, в чём причина пока не ясно. 
-Предупреждение 'Exponentiation operator' is only available in ES7 
-(use 'esversion: 7'). (W119) устранил, но не помогло.*/
-
-//console.log(subResult.join("") );
-
-alert(subResult[3]); 
-
-
-
+alert((subResult[0] + subResult[1]));
+ 
 }
 main();
