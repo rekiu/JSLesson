6484096c8ba1f66cd8e555c72d398ed86dffdc0b
Ivan <rekiu@yandex.ru>
2019-05-08 22:16:08 +0800

    9 задание

---
+++ lesson-9/normal_2/script.js
@@ -0,0 +1,8 @@
+let age = document.getElementById('age');
+ 
+function showUser(surname, name) {
+  this.value = age.value;
+         alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+}
+ 
+showUser('Джигурда', 'Никита');
